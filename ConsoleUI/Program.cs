e52205ce77ec4fca7198ec3e7502c8d747d69af8
Orhan <orhan1766@gmail.com>
2021-02-12 21:49:01 +0300

    9.Ders Ödev

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,15 +10,52 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Car car1 = new Car { BrandId = 1, ColorId = 5, DailyPrice = 100, ModelYear = 2019, Description = "Focus" };
-            Car car2 = new Car { BrandId = 1, ColorId = 4, DailyPrice = 150, ModelYear = 2019, Description = "Passat" };
-            Car car3 = new Car { BrandId = 3, ColorId = 3, DailyPrice = 120, ModelYear = 2019, Description = "Jetta" };
+            Car car1 = new Car { BrandId = 1, ColorId = 5, CarName = "FORD FOCUS", DailyPrice = 120, ModelYear = 2019, Description = "Düz Vites" };
+            Brand brand1 = new Brand { Name = "FORD" };
+            Color color1 = new Color { Name = "Beyaz" };
+
+            Car car2 = new Car { BrandId = 2, ColorId = 4, CarName = "RENAULT MEGANE", DailyPrice = 150, ModelYear = 2020, Description = "Otomatik Vites" };
+            Brand brand2 = new Brand { Name = "RENAULT" };
+            Color color2 = new Color { Name = "Gri" };
+
+            Car car3 = new Car { BrandId = 3, ColorId = 3, CarName = "TOYOTA COROLLA", DailyPrice = 100, ModelYear = 2018, Description = "Düz Vites" };
+            Brand brand3 = new Brand { Name = "TOYOTA" };
+            Color color3 = new Color { Name = "Mavi" };
+
+            Car car4 = new Car { BrandId = 1, ColorId = 4, CarName = "FORD FOCUS", DailyPrice = 140, ModelYear = 2019, Description = "Otomatik Vites" };
+            Brand brand4 = new Brand { Name = "FORD" };
+            Color color4 = new Color { Name = "Gri" };
 
             CarManager carManager = new CarManager(new EfCarDal());
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            ColorManager colorManager = new ColorManager(new EfColorDal());
 
-            foreach (var car in carManager.GetCarsByColorId(5))
+            carManager.Add(car1);
+            brandManager.Add(brand1);
+            colorManager.Add(color1);
+
+            carManager.Add(car2);
+            brandManager.Add(brand2);
+            colorManager.Add(color2);
+
+            carManager.Add(car3);
+            brandManager.Add(brand3);
+            colorManager.Add(color3);
+
+            carManager.Add(car4);
+            brandManager.Add(brand4);
+            colorManager.Add(color4);
+
+            foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine(car.Description);
+                Console.WriteLine(car.CarName+" / "+car.Description);
+            }
+
+            //carManager.Delete(new Car { Id=1});
+
+            foreach (var detail in carManager.GetCarDetail())
+            {
+                Console.WriteLine(detail.CarName + " / " +detail.BrandName + " / " +detail.ColorName + " / " +detail.DailyPrice);
             }
         }
     }
--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess;
 using Core.DataAccess.EntityFramework;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +10,6 @@ namespace DataAccess.Abstract
 {
     public interface ICarDal:IEntityRepository<Car>
     {
-        
+        List<ProductDetailDto> GetProductDetails(); 
     }
 }
