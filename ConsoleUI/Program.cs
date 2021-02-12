d122a09864e58887dcc18543ec8288fdd2ac7502
Orhan <orhan1766@gmail.com>
2021-02-12 19:22:01 +0300

    saat 19:20

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,22 +10,16 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Car car1 = new Car {BrandId = 1, ColorId = 5, DailyPrice = 100, ModelYear = 2019, Description = "Focus" };
-            Car car2 = new Car {BrandId = 1, ColorId = 4, DailyPrice = 150, ModelYear = 2019, Description = "Passat" };
-            Car car3 = new Car {BrandId = 3, ColorId = 3, DailyPrice = 120, ModelYear = 2019, Description = "Jetta" };
+            Car car1 = new Car { BrandId = 1, ColorId = 5, DailyPrice = 100, ModelYear = 2019, Description = "Focus" };
+            Car car2 = new Car { BrandId = 1, ColorId = 4, DailyPrice = 150, ModelYear = 2019, Description = "Passat" };
+            Car car3 = new Car { BrandId = 3, ColorId = 3, DailyPrice = 120, ModelYear = 2019, Description = "Jetta" };
 
             CarManager carManager = new CarManager(new EfCarDal());
 
-            carManager.Add(car1);
-            carManager.Add(car2);
-            carManager.Add(car3);
-
-            foreach (var car in carManager.GetAll())
+            foreach (var car in carManager.GetCarsByColorId(5))
             {
                 Console.WriteLine(car.Description);
             }
-
-            
         }
     }
 }
